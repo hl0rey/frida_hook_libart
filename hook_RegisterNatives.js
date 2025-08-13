bfdc7f862241ab37d6dc15e50d7c701070bf53b7
hl0rey <hl0rey@163.com>
2025-08-13 15:14:49 +0800

    按17的api修改脚本，加入友好报错

--- hook_RegisterNatives.js
+++ hook_RegisterNatives.js
@@ -1,6 +1,26 @@
 
 function find_RegisterNatives(params) {
-    let symbols = Module.enumerateSymbolsSync("libart.so");
+    console.log("[!]find_RegisterNatives start");
+    var module = Process.findModuleByName("libart.so");
+    if (module == null) {
+        console.log("[X]libart.so not found");
+        return;
+    }else{
+        console.log("[!]libart.so base:", module.base);
+        console.log("[!]libart.so size:", module.size);
+        console.log("[!]libart.so path:", module.path);
+    }
+    
+
+    let symbols = module.enumerateSymbols();
+    if (symbols.length==0) {
+        console.log("[X]symbols is empty,so maybe stripped.");
+        console.log("[!]try to get Exports.")
+        symbols = module.enumerateExports();
+        console.log("[!]Exports length:",symbols.length)
+    }
+    
+
     let addrRegisterNatives = null;
     for (let i = 0; i < symbols.length; i++) {
         let symbol = symbols[i];
@@ -11,7 +31,7 @@ function find_RegisterNatives(params) {
                 symbol.name.indexOf("RegisterNatives") >= 0 && 
                 symbol.name.indexOf("CheckJNI") < 0) {
             addrRegisterNatives = symbol.address;
-            console.log("RegisterNatives is at ", symbol.address, symbol.name);
+            console.log("[*]RegisterNatives is at ", symbol.address, symbol.name);
             hook_RegisterNatives(addrRegisterNatives)
         }
     }
@@ -23,23 +43,24 @@ function hook_RegisterNatives(addrRegisterNatives) {
     if (addrRegisterNatives != null) {
         Interceptor.attach(addrRegisterNatives, {
             onEnter: function (args) {
-                console.log("[RegisterNatives] method_count:", args[3]);
+                console.log("[!][RegisterNatives] onEnter",addrRegisterNatives);
+
+                console.log("[*][RegisterNatives] method_count:", args[3]);
                 let java_class = args[1];
                 let class_name = Java.vm.tryGetEnv().getClassName(java_class);
-                //console.log(class_name);
+                console.log("[*][RegisterNatives] class_name:", class_name);
 
                 let methods_ptr = ptr(args[2]);
 
                 let method_count = parseInt(args[3]);
                 for (let i = 0; i < method_count; i++) {
-                    let name_ptr = Memory.readPointer(methods_ptr.add(i * Process.pointerSize * 3));
-                    let sig_ptr = Memory.readPointer(methods_ptr.add(i * Process.pointerSize * 3 + Process.pointerSize));
-                    let fnPtr_ptr = Memory.readPointer(methods_ptr.add(i * Process.pointerSize * 3 + Process.pointerSize * 2));
-
-                    let name = Memory.readCString(name_ptr);
-                    let sig = Memory.readCString(sig_ptr);
+                    let name_ptr = methods_ptr.add(i * Process.pointerSize * 3).readPointer();
+                    let sig_ptr = methods_ptr.add(i * Process.pointerSize * 3 + Process.pointerSize).readPointer();
+                    let fnPtr_ptr = methods_ptr.add(i * Process.pointerSize * 3 + Process.pointerSize * 2).readPointer();
+                    let name = name_ptr.readCString();
+                    let sig = sig_ptr.readCString();
                     let symbol = DebugSymbol.fromAddress(fnPtr_ptr)
-                    console.log("[RegisterNatives] java_class:", class_name, "name:", name, "sig:", sig, "fnPtr:", fnPtr_ptr,  " fnOffset:", symbol, " callee:", DebugSymbol.fromAddress(this.returnAddress));
+                    console.log("[*][RegisterNatives] java_class:", class_name, "name:", name, "sig:", sig, "fnPtr:", fnPtr_ptr,  " fnOffset:", symbol, " callee:", DebugSymbol.fromAddress(this.returnAddress));
                 }
             }
         });
